3b9a4dc9f4a621c9bfbd91fd1d0211fa3f25b0cf
Ahmad Sadati <a.sadati336@gmail.com>
2023-11-14 17:11:03 +0330

    cart list add and remove

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
+const shopListShow= document.querySelector(".showList");
 let pList = '';
 products.forEach((val, index) => {
     pList += `
@@ -24,10 +24,38 @@ products.forEach((val, index) => {
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="https://digikala.com${val.url.uri}">Shop It</a></div>
+                                <div class="text-center"><button class="btn btn-primary" onclick="addToCart(${index})" >shop it</button></div>
                             </div>
                         </div>
                     </div>`;
 })
 
 document.querySelector(".productList").innerHTML = pList;
+
+const shopList = [];
+function addToCart(index){
+   
+    let val = products[index]
+    console.log(val);
+    console.log(index);
+    shopList.push({
+        title:val.title_fa,
+        image:val.images.main.url[0],
+        price:val.default_variant.price.selling_price.toLocaleString()
+    })
+    showListShop();
+
+}
+
+function showListShop() {
+    let helper;
+    shopList.forEach((value,index) =>{
+        helper+= `<li><button onclick=removeCart(${index})>Remove</button><img style="height:60px" src="${value.image}"/><span>${value.price}" | "${value.title} </span></li>`
+    })
+    shopListShow.innerHTML = helper;
+
+}
+function removeCart (index) {
+    shopList.splice(index , 1);
+    showListShop()
+}
